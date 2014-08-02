9d359be9fc96398169098e46556250837c594eb6
ZombZ <player2@zombz.net>
2014-08-02 01:54:18 +0100

    Added: OW Loot Files Prior To Merge

---
+++ CustomLootTables/CustomOW/CfgLoot.hpp
@@ -0,0 +1,204 @@
+class CfgLoot {
+
+	trash[] = {
+			{"TrashTinCan",0.02},
+			{"TrashJackDaniels",0.10},
+			{"ItemSodaEmpty",0.50},
+			{"ItemTrashToiletpaper",0.40},
+			{"ItemTrashRazor",0.20}
+	};
+	
+	civilian[] = {
+			{"ItemSodaCoke",0.12},
+			{"ItemSodaPepsi",0.09},
+			{"FoodCanBakedBeans",0.05},
+			{"FoodCanSardines",0.05},
+			{"FoodCanFrankBeans",0.05},
+			{"FoodCanPasta",0.05},
+			{"2Rnd_shotgun_74Slug",0.05},
+			{"2Rnd_shotgun_74Pellets",0.05},
+			{"ItemBandage",0.06},
+			{"ItemPainkiller",0.06},
+			{"RH_8Rnd_45cal_m1911",0.02},
+			{"RH_13Rnd_9x19_bhp",0.02},
+			{"RH_8Rnd_9x19_Mksd",0.01},
+			{"RH_20Rnd_9x19_M93",0.02},
+			{"RH_7Rnd_32cal_ppk",0.02}
+	};
+	
+	food[] = {
+			{"ItemSodaCoke",0.06},
+			{"ItemSodaPepsi",0.07},
+			{"ItemSodaMtngreen",0.03},
+			{"FoodCanBakedBeans",0.15},
+			{"FoodCanSardines",0.06},
+			{"FoodCanFrankBeans",0.06}
+	};
+	
+	generic[] = {
+			{"ItemSodaCoke",0.06},
+			{"ItemSodaPepsi",0.04},
+			{"FoodCanBakedBeans",0.04},
+			{"FoodCanSardines",0.05},
+			{"FoodCanFrankBeans",0.01},
+			{"ItemWaterbottleUnfilled",0.01},
+			{"ItemWaterbottle",0.01},
+			{"ItemBandage",0.10},
+			{"2Rnd_shotgun_74Slug",0.05},
+			{"2Rnd_shotgun_74Pellets",0.05},
+			{"15Rnd_W1866_Slug",0.02},
+			{"5x_22_LR_17_HMR",0.01},
+			{"WoodenArrow",0.01},
+			{"HandRoadFlare",0.05},
+			{"ItemPainkiller",0.02},
+			{"HandChemGreen",0.01},
+			{"HandChemBlue",0.01},
+			{"HandChemRed",0.01},
+			{"RH_8Rnd_45cal_m1911",0.03},
+			{"RH_13Rnd_9x19_bhp",0.03},
+			{"RH_8Rnd_9x19_Mksd",0.01},
+			{"RH_20Rnd_9x19_M93",0.02},
+			{"RH_7Rnd_32cal_ppk",0.02},
+			{"DDOPP_1Rnd_X26",0.02}
+	};
+
+	medical[] = {
+			{"ItemBandage",0.80},
+			{"ItemPainkiller",0.50},
+			{"ItemMorphine",0.50},
+			{"ItemEpinephrine",0.20},
+			{"ItemAntibiotic",0.10}
+	};
+	
+	hospital[] = {
+			{"ItemBandage",0.33},
+			{"ItemPainkiller",0.17},
+			{"ItemMorphine",0.13},
+			{"ItemEpinephrine",0.09},
+			{"ItemBloodbag",0.17},
+			{"ItemAntibiotic",0.06}
+	};
+	
+	military[] = {
+			{"ItemSodaCoke",0.01},
+			{"ItemSodaPepsi",0.01},
+			{"ItemBandage",0.04},
+			{"ItemAntibiotic",0.01},
+			{"ItemPainkiller",0.04},
+			{"ItemMorphine",0.01},
+			{"30Rnd_556x45_Stanag",0.04},
+			{"20Rnd_762x51_DMR",0.04},
+			{"15Rnd_9x19_M9SD",0.01},
+			{"15Rnd_9x19_M9",0.02},
+			{"30Rnd_762x39_AK47",0.04},
+			{"30Rnd_545x39_AK",0.04},
+			{"5Rnd_762x51_M24",0.02},
+			{"8Rnd_B_Beneli_74Slug",0.04},
+			{"1Rnd_HE_M203",0.01},
+			{"1Rnd_Smoke_M203",0.01},
+			{"200Rnd_556x45_M249",0.02},
+			{"HandGrenade_west",0.01},
+			{"SmokeShell",0.01},
+			{"8Rnd_B_Beneli_Pellets",0.04},
+			{"30Rnd_556x45_StanagSD",0.01},
+			{"30Rnd_9x19_MP5",0.02},
+			{"30Rnd_9x19_MP5SD",0.01},
+			{"100Rnd_762x51_M240",0.01},
+			{"HandChemBlue",0.01},
+			{"HandChemRed",0.01},
+			{"FoodMRE",0.03},
+			{"20rnd_762x51_B_SCAR",0.01},
+			{"10Rnd_9x39_SP5_VSS",0.01},
+			{"10Rnd_762x54_SVD",0.01},
+			{"100Rnd_762x54_PK",0.01},
+			{"30Rnd_762x39_SA58",0.04},
+			{"200Rnd_762x54_PKT",0.01},
+			{"30Rnd_556x45_G36",0.02},
+			{"30Rnd_556x45_G36SD",0.01},
+			{"8Rnd_B_Saiga12_74Slug",0.03},
+			{"30Rnd_9x19_UZI_SD",0.02},
+			{"30Rnd_9x19_UZI",0.03},
+			{"RH_6Rnd_357_Mag",0.03},
+			{"RH_15Rnd_9x19_uspsd",0.02},
+			{"RH_7Rnd_50_AE",0.01},
+			{"RH_12Rnd_45cal_usp",0.03},
+			{"RH_8Rnd_9x19_Mksd",0.02},
+			{"RH_30Rnd_9x19_tec",0.03},
+			{"RH_17Rnd_9x19_g17",0.03},
+			{"RH_20Rnd_32cal_vz61",0.03},
+			{"RH_6Rnd_44_Mag",0.03},
+			{"FHQ_rem_5Rnd_338Lapua_MSR_T",0.01},
+			{"FHQ_rem_5Rnd_300Win_XM2010_T",0.01},
+			{"FHQ_rem_30Rnd_680x43_ACR",0.02},
+			{"FHQ_rem_20Rnd_680x43_ACR",0.02},
+			{"FHQ_rem_30Rnd_680x43_ACR_T_SD",0.01},
+			{"FHQ_rem_20Rnd_680x43_ACR_T_SD",0.02},
+			{"FHQ_rem_10Rnd_338Lapua_MSR_T",0.01},
+			{"FHQ_rem_5Rnd_300Win_XM2010_T",0.01},
+			{"VIL_30Rnd_556x45_HK",0.02},
+			{"VIL_35Rnd_556x45_G",0.02},
+			{"5Rnd_127x108_KSVK",0.01},
+			{"FHQ_rem_20Rnd_762x51_PMAG_T",0.02},
+			{"FHQ_rem_20Rnd_762x51_PMAG_T_SD",0.02},
+			{"5Rnd_86x70_L115A1",0.01},
+			{"200Rnd_556x45_L110A1",0.02},
+			{"RH_20Rnd_762x51_hk417",0.02},
+			{"20Rnd_556x45_Stanag",0.03},
+			{"5Rnd_86x70_L115A1",0.01},
+			{"5Rnd_86x70_L115A1",0.01},
+			{"5Rnd_86x70_L115A1",0.01},
+	};
+	
+	militaryWEST[] = {
+			{"ItemBandage",0.06},
+			{"ItemAntibiotic",0.01},
+			{"ItemPainkiller",0.06},
+			{"ItemMorphine",0.02},
+			{"ItemWaterbottleUnfilled",0.02},
+			{"30Rnd_556x45_Stanag",0.04},
+			{"17Rnd_9x19_glock17",0.05},
+			{"7Rnd_45ACP_1911",0.05},
+			{"15Rnd_9x19_M9",0.02},
+			{"8Rnd_B_Beneli_74Slug",0.04},
+			{"1Rnd_HE_M203",0.01},
+			{"1Rnd_Smoke_M203",0.01},	
+			{"HandGrenade_west",0.01},
+			{"SmokeShell",0.01},
+			{"HandChemGreen",0.01},
+			{"HandChemBlue",0.01},
+			{"30Rnd_9x19_MP5",0.02},
+			{"FoodMRE",0.01}
+	};
+
+	militaryEAST[] = {
+			{"ItemBandage",0.06},
+			{"ItemAntibiotic",0.01},
+			{"ItemPainkiller",0.06},
+			{"ItemMorphine",0.02},
+			{"HandGrenade_east",0.01},
+			{"75Rnd_545x39_RPK",0.03},
+			{"30Rnd_762x39_AK47",0.04},
+			{"30Rnd_545x39_AK",0.04},
+			{"8Rnd_B_Beneli_74Slug",0.08},
+			{"30Rnd_9x19_UZI",0.02},
+			{"SmokeShell",0.01}
+	};
+
+	policeman[] = {
+			{"ItemBandage",0.99},
+			{"15Rnd_W1866_Slug",0.30},
+			{"8Rnd_B_Beneli_74Slug",0.50},
+			{"HandRoadFlare",0.01},
+			{"RH_6Rnd_44_Mag",0.20}
+	};
+	
+	hunter[] = {
+			{"ItemBandage",0.25},
+			{"30Rnd_556x45_Stanag",0.15},
+			{"5Rnd_762x51_M24",0.10},
+			{"5x_22_LR_17_HMR",0.06},
+			{"10x_303",0.03},
+			{"ItemWaterbottleUnfilled",0.10},
+			{"FoodMRE",0.09}
+		};
+};
