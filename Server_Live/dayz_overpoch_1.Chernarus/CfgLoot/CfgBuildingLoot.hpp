501e3ad22911c6d1e09a4bd999e0e5bcc9846a1b
ZombZ <player2@zombz.net>
2015-01-31 08:41:24 +0000

    Loot Rebalanced

--- Server_Live/dayz_overpoch_1.Chernarus/CfgLoot/CfgBuildingLoot.hpp
+++ Server_Live/dayz_overpoch_1.Chernarus/CfgLoot/CfgBuildingLoot.hpp
@@ -26,21 +26,10 @@
                  lootChance = 0.6;
                  lootPos[] = {};
                  lootType[] = {
-                 //one spawn
-                 //tools
-                         //{"ItemMatchbox","weapon",0.01},
                          {"ItemKnife","weapon",0.01},
-                 //weapons
                          {"WeaponHolder_ItemHatchet","object",0.01},
-                 //special
                          {"WeaponHolder_ItemTent","object",0.01},
-                         //{"WeaponHolder_DomeTentStorage","object",0.01},
-                 //mags special
                          {"","food",0.2},
-                 //bags
-                         //{"DZ_Patrol_Pack_EP1","object",0.01},
-                 //Multiple spawns
-                         //{"","Residential",0.60},
                          {"", "AKHighWeaps",  0.06}, 
                          {"militarybackpacks","militarybackpacks",0.01},
                          {"", "SVDWeaps",  0.01}, 
@@ -61,17 +50,16 @@
                  lootChance = 0.6;
                  lootPos[] = {};
                  lootType[] = {
-                         {"ItemKnife","generic",                        0.15},
-                         {"ItemMatchbox_DZE","generic",                 0.15},
-                         {"backpacks","backpacks",                      0.15},
-                         {"tents","tents",                              0.15},
-                         {"PartPlywoodPack","magazine",                 0.15},
+                         {"","civilian",                                0.30},
+                         {"","generic",                                 0.20},
+                         {"","food",                                    0.15},
                          {"", "MeleeWeaps",                             0.15}, 
-                         {"clothes","clothes",                          0.02},
-                         {"specialclothes","specialclothes",            0.01},
-                         {"", "PistolsLow",                             0.03}, 
+                         {"backpacks","backpacks",                      0.06},
+                         {"clothes","clothes",                          0.04},
+                         {"", "PistolsLow",                             0.03},
+                         {"tents","tents",                              0.02},
                          {"", "ShotgunWeaps",                           0.03}, 
-                         {"", "RiflesLowWeaps",                         0.01}
+                         {"", "RiflesLowWeaps",                         0.02}
                  };
                  lootTypeSmall[] = {
                          { "ItemSodaMdew","magazine",                   0.1 },
@@ -121,11 +109,11 @@
                          { "", "PistolsLow",  0.08}, 
                          { "ItemKnife","weapon",0.05 },
                          { "ItemMatchbox_DZE","weapon",0.03 },
-                         { "","generic",0.31 },                  
+                         { "","generic",0.01 },                  
                          { "","military",0.04 },
                          { "","trash",0.03 },
-                         { "","office",0.03 },
-                         { "","office2",0.03 },
+                         { "","office",0.15 },
+                         { "","office2",0.15 },
                          { "clothes","single",0.16 },
                          { "specialclothes","single",0.01 },
                          { "ItemToolbox","weapon",0.01},
@@ -139,25 +127,45 @@
                  lootChance = 0.6;
                  lootPos[] = {};
                  lootType[] = {
-                         {"ItemGenerator","magazine",0.01},
-                         {"ItemFuelBarrelEmpty","magazine",0.01},
-                         {"PartGeneric","magazine",0.04},
-                         {"PartWheel","magazine",0.05},
-                         {"backpacks","backpacks",0.01},
-                         {"PartFueltank","magazine",0.02},
-                         {"PartEngine","magazine",0.02},
-                         {"PartGlass","magazine",0.04},
-                         {"PartVRotor","magazine",0.01},
-                         {"ItemJerrycan","magazine",0.04},
-                         {"WeaponHolder_ItemHatchet_DZE","object",0.07},
-                         {"ItemToolbox","weapon",0.06},
-                         {"ItemWire","magazine",0.01},
-                         {"ItemTankTrap","magazine",0.04},
-                         {"ItemKeyKit","weapon",0.01},
-                         {"CinderBlocks","magazine",0.03},
-                         {"", "PistolsLow",  0.08}, 
-                         {"", "MeleeWeaps",  0.01}, 
-                         {"MortarBucket","magazine",0.01}
+                        //30%
+                        {"PartGeneric","magazine",0.09},
+                        {"PartWheel","magazine",0.06},
+                        {"PartGlass","magazine",0.05},
+                        {"PartEngine","magazine",0.04},
+                        {"PartFueltank","magazine",0.04},
+                        {"PartVRotor","magazine",0.02},
+                        //30%
+                        {"ItemPole","magazine",0.03},
+                        {"ItemKiloHemp","magazine",0.01},
+                        {"ItemCanvas","magazine",0.02},
+                        {"ItemDocument","magazine",0.03},
+                        {"ItemBurlap","magazine",0.02},
+                        {"ItemCorrugated","magazine",0.02},
+                        {"PartWoodLumber","magazine",0.02},
+                        {"PartWoodPlywood","magazine",0.02},
+                        {"MortarBucket","magazine",0.02},
+                        {"CinderBlocks","magazine",0.02},
+                        {"ItemGenerator","magazine",0.02},
+                        {"ItemTankTrap","magazine",0.02},
+                        {"ItemWire","magazine",0.02},
+                        {"ItemSandbag","magazine",0.02},
+                        {"ItemDocumentRamp","magazine",0.01},
+                        //20%
+                        {"", "generic",0.20},
+                        //5%
+                        {"ItemMixOil","magazine",0.01},
+                        {"ItemJerrycanEmpty","magazine",0.03},
+                        {"ItemJerrycan","magazine",0.01},
+                        //5%
+                        {"ItemToolbox","weapon",0.04},
+                        {"ItemKeyKit","weapon",0.01},
+                        //5%
+                        {"", "PistolsLow",0.01}, 
+                        {"", "MeleeWeaps",0.04},
+                        //4%
+                        {"", "trash", 0.04},
+                        //1%
+                        {"backpacks","backpacks",0.01}
                  };
                  lootTypeSmall[] = {
                          { "","generic",0.15 },
@@ -175,24 +183,29 @@
                  lootChance = 0.6;
                  lootPos[] = {};
                  lootType[] = {
-                         {"ItemGenerator","magazine",                   0.01},
-                         {"fuel_pump_kit","magazine",                   0.01},
-                         {"backpacks","backpacks",                      0.01},
-                         {"PartGeneric","magazine",                     0.04},
-                         {"PartWheel","magazine",                       0.05},
-                         {"PartFueltank","magazine",                    0.02},
-                         {"PartEngine","magazine",                      0.02},
-                         {"PartGlass","magazine",                       0.04},
-                         {"PartVRotor","magazine",                      0.01},
-                         {"ItemJerrycan","magazine",                    0.04},
-                         {"WeaponHolder_ItemHatchet_DZE","object",      0.07},
-                         {"ItemKnife","military",                       0.07},
-                         {"ItemToolbox","weapon",                       0.06},
-                         {"ItemWire","magazine",                        0.01},
-                         {"ItemTankTrap","magazine",                    0.04},
-                         {"", "PistolsLow",                             0.08}, 
-                         {"", "MeleeWeaps",                             0.01}, 
-                         {"ItemKeyKit","weapon",                        0.01}
+                        //30%
+                        {"PartGeneric","magazine",0.09},
+                        {"PartWheel","magazine",0.06},
+                        {"PartGlass","magazine",0.05},
+                        {"PartEngine","magazine",0.04},
+                        {"PartFueltank","magazine",0.04},
+                        {"PartVRotor","magazine",0.02},
+                        //30%
+                        {"", "generic",0.30},
+                        //10%
+                        {"ItemMixOil","magazine",0.02},
+                        {"ItemJerrycanEmpty","magazine",0.06},
+                        {"ItemJerrycan","magazine",0.02},
+                        //10%
+                        {"ItemToolbox","weapon",0.08},
+                        {"ItemKeyKit","weapon",0.02},
+                        //5%
+                        {"", "PistolsLow",0.01}, 
+                        {"", "MeleeWeaps",0.04},
+                        //4%
+                        {"", "trash", 0.04},
+                        //1%
+                        {"backpacks","backpacks",0.01}
                  };
                  lootTypeSmall[] = {
                          { "","generic",                                0.2  },
@@ -216,10 +229,10 @@
                          {"ItemFuelBarrelEmpty","magazine",     0.01},
                          {"", "PistolsLow",                     0.03}, 
                          {"", "SniperLowWeaps",                 0.01}, 
-                         {"", "RiflesLowWeaps",                 0.01}, 
-                         {"", "ShotgunWeaps",                   0.02}, 
+                         {"", "RiflesLowWeaps",                 0.02}, 
+                         {"", "ShotgunWeaps",                   0.04}, 
                          {"backpacks","backpacks",              0.03},
-                         {"", "MeleeWeaps",                     0.2}, 
+                         {"", "MeleeWeaps",                     0.17}, 
                          {"ItemSledgeHandle","magazine",        0.01}
                  };
                  lootTypeSmall[] = {
@@ -238,20 +251,12 @@
                  zombieChance = 0.5;
                  zombieClass[] = {"zZombie_Base","zZombie_Base","z_teacher","z_suit1","z_suit2"};
                  lootType[] = {
-                         {"ItemSodaMdew","magazine",            0.1},
-                         {"ItemSodaRbull","magazine",           0.1},
-                         {"ItemSodaOrangeSherbet","magazine",   0.1},
-                         {"ItemFlashlight","generic",           0.1},
-                         {"ItemKnife","generic",                0.1},
-                         {"ItemToolbox","weapon",               0.1},
-                         {"ItemMatchbox_DZE","generic",         0.1},
-                         {"backpacks","backpacks",              0.1},
-                         {"tents","tents",                      0.1},
-                         {"", "PistolsLow",                     0.01}, 
-                         {"", "ShotgunWeaps",                   0.01}, 
-                         {"PartPlywoodPack","magazine",         0.08}
-
-
+                         { "","generic",                        0.3},
+                         { "","food",                           0.3},
+                         {"backpacks","backpacks",              0.06},
+                         {"tents","tents",                      0.04},
+                         {"", "PistolsLow",                     0.15}, 
+                         {"", "ShotgunWeaps",                   0.15}
                  };
                  lootTypeSmall[] = {
                          {"", "PistolsLow",                     0.01 }, 
@@ -336,7 +341,7 @@
                  lootChance = 0.6;
                  lootPos[] = {};
                  lootType[] = {
-                         {"", "MeleeWeaps",  0.04}, 
+                         {"", "MeleeWeaps",  0.02}, 
                          {"", "PistolMilWeaps",  0.1}, 
                          {"", "ACRLowWeaps",  0.05}, 
                          {"", "ScarLowWeaps",  0.05}, 
@@ -356,13 +361,13 @@
                          {"PartGeneric","magazine",0.03},
                          {"ItemGenerator","magazine",0.01},
                          {"PartWheel","magazine",0.02},
-                         {"ItemFlashlightRed","military",0.03},
-                         {"ItemKnife","military",0.04},
-                         {"ItemGPS","weapon",0.01},
-                         {"PartVRotor","magazine",0.01},
-                         {"militarybackpacks","militarybackpacks",0.03},
-                         {"ItemEtool","weapon",0.03},
-                         {"ItemSandbag","magazine",0.04},
+                         {"ItemFlashlightRed","military",0.01},
+                         {"ItemKnife","military",0.02},
+                         {"ItemGPS","weapon",0.02},
+                         {"PartVRotor","magazine",0.03},
+                         {"militarybackpacks","militarybackpacks",0.02},
+                         {"ItemEtool","weapon",0.02},
+                         {"ItemSandbag","magazine",0.02},
                          {"ItemFuelBarrelEmpty","magazine",0.01},
                          {"fuel_pump_kit","magazine",0.01}
                  };
@@ -386,8 +391,8 @@
                  lootChance = 0.6;
                  lootPos[] = {};
                  lootType[] = {
-                         {"", "MeleeWeaps",  0.08}, 
-                         {"", "PistolMilWeaps",  0.08}, 
+                         {"", "MeleeWeaps",  0.02}, 
+                         {"", "PistolMilWeaps",  0.1}, 
                          {"", "ACRLowWeaps",  0.05}, 
                          {"", "ScarLowWeaps",  0.05}, 
                          {"", "HK416Weaps",  0.05}, 
@@ -400,20 +405,19 @@
                          {"", "M4Weaps",  0.05}, 
                          {"", "M8Weaps",  0.05}, 
                          {"", "SubWeaps",  0.04}, 
-                         {"", "AKHighWeaps",  0.02}, 
+                         {"", "AKHighWeaps",  0.08}, 
                          {"", "SniperMedWeaps",  0.01}, 
                          {"", "RiflesMedWeaps",  0.04}, 
                          {"PartGeneric","magazine",0.03},
                          {"ItemGenerator","magazine",0.01},
                          {"PartWheel","magazine",0.02},
-                         {"ItemFlashlightRed","military",0.03},
-                         {"ItemKnife","military",0.04},
-                         {"ItemGPS","weapon",0.01},
-                         {"PartVRotor","magazine",0.01},
-                         {"militarybackpacks","militarybackpacks",0.03},
-                         {"militaryclothes","militaryclothes",0.05 },
-                         {"ItemEtool","weapon",0.03},
-                         {"ItemSandbag","magazine",0.04},
+                         {"ItemFlashlightRed","military",0.01},
+                         {"ItemKnife","military",0.02},
+                         {"ItemGPS","weapon",0.02},
+                         {"PartVRotor","magazine",0.03},
+                         {"militarybackpacks","militarybackpacks",0.02},
+                         {"ItemEtool","weapon",0.02},
+                         {"ItemSandbag","magazine",0.02},
                          {"ItemFuelBarrelEmpty","magazine",0.01},
                          {"fuel_pump_kit","magazine",0.01}
                  };
@@ -440,32 +444,32 @@
                  lootPos[] = {};
                  lootType[] = {
                          {"", "PistolMilWeaps",  0.1}, 
-                         {"", "ACRLowWeaps",  0.01}, 
-                         {"", "ACRHighWeaps",  0.06}, 
-                         {"", "G36Weaps",  0.08}, 
-                         {"", "L85Weaps",  0.08}, 
-                         {"", "M4Weaps",  0.08}, 
-                         {"", "M8Weaps",  0.08}, 
-                         {"", "AKHighWeaps",  0.08}, 
-                         {"", "RiflesMedWeaps",  0.07}, 
-                         {"", "ScarHighWeaps",  0.06}, 
-                         {"", "HK417Weaps",  0.06}, 
-                         {"", "LMGWeaps",  0.03}, 
-                         {"", "HMGWeaps",  0.05}, 
-                         {"", "SniperMedWeaps",  0.05}, 
-                         {"", "LauncherWeaps",  0.04},
+                         {"", "ACRLowWeaps",  0.03}, 
+                         {"", "ACRHighWeaps",  0.04}, 
+                         {"", "G36Weaps",  0.04}, 
+                         {"", "L85Weaps",  0.04}, 
+                         {"", "M4Weaps",  0.04}, 
+                         {"", "M8Weaps",  0.03}, 
+                         {"", "AKHighWeaps",  0.07}, 
+                         {"", "RiflesMedWeaps",  0.06}, 
+                         {"", "ScarHighWeaps",  0.05}, 
+                         {"", "HK417Weaps",  0.05}, 
+                         {"", "LMGWeaps",  0.04}, 
+                         {"", "HMGWeaps",  0.03}, 
+                         {"", "SniperMedWeaps",  0.06}, 
+                         {"", "LauncherWeaps",  0.03},
                          {"", "RocketWeaps",  0.02},
                          {"", "UltraWeaps",  0.01},
                          {"", "PistolTopWeaps",  0.01}, 
                          {"", "SniperTopWeaps",  0.01},
-                         {"AmmoBoxSmall_556","object",0.05},
-                         {"AmmoBoxSmall_762","object",0.05},
-                         {"ItemFlashlightRed","military",0.02},
-                         {"ItemGPS","weapon",0.03},
-                         {"Binocular_Vector","military",0.03},
-                         {"militarybackpacks","militarybackpacks",0.06},
-                         {"militaryammo","militaryammo",0.05},
-                         {"machinegunammo","machinegunammo",0.05},
+                         {"AmmoBoxSmall_556","object",0.02},
+                         {"AmmoBoxSmall_762","object",0.02},
+                         {"ItemFlashlightRed","military",0.01},
+                         {"ItemGPS","weapon",0.01},
+                         {"Binocular_Vector","military",0.02},
+                         {"militarybackpacks","militarybackpacks",0.02},
+                         {"militaryammo","militaryammo",0.06},
+                         {"machinegunammo","machinegunammo",0.03},
                          {"militaryclothes","militaryclothes",0.05}
                  };
                  lootTypeSmall[] = {
@@ -499,7 +503,7 @@
                          {"", "AKHighWeaps",  0.06}, 
                          {"", "SVDWeaps",  0.01}, 
                          {"", "AKLowWeaps",  0.09}, 
-                         {"militarybackpacks","militarybackpacks",0.02},
+                         {"militarybackpacks","militarybackpacks",0.01},
                          {"militaryclothes","militaryclothes",0.01 },
                          {"", "ShotgunWeaps",  0.03}, 
                          {"", "SniperLowWeaps",  0.02}, 
