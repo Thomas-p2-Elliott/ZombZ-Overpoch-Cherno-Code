e1d2c917b86658b7b55402d40c78143c6747c9f8
ZombZ <player2@zombz.net>
2015-02-23 03:31:20 +0000

    ViewDist Updates

--- Server_Live/dayz_overpoch_1.Chernarus/configs/rc.hpp
+++ Server_Live/dayz_overpoch_1.Chernarus/configs/rc.hpp
@@ -22,35 +22,35 @@ class ExtraRc {
 		};
 		class distance400m {
 			text = "400 Meters";
-			script = "setViewDistance 400; systemChat('ViewDistance: 400'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 400; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 400'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance600m {
 			text = "600 Meters";
-			script = "setViewDistance 600; systemChat('ViewDistance: 600'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 600; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 600'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance800m {
 			text = "800 Meters";
-			script = "setViewDistance 800; systemChat('ViewDistance: 800'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 800; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 800'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance1000m {
 			text = "1000 Meters";
-			script = "setViewDistance 1000; systemChat('ViewDistance: 1000'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 1000; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 1000'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance1250m {
 			text = "1250 Meters";
-			script = "setViewDistance 1250; systemChat('ViewDistance: 1250'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 1250; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 1250'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance1500m {
 			text = "1500 Meters";
-			script = "setViewDistance 1500; systemChat('ViewDistance: 1500'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 1500; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 1500'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance2000m {
 			text = "2000 Meters";
-			script = "setViewDistance 2000; systemChat('ViewDistance: 2000'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 2000; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 2000'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance3000m {
 			text = "3000 Meters";
-			script = "setViewDistance 3000; systemChat('ViewDistance: 3000'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 3000; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 3000'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 	};
 	class Binocular {
@@ -60,35 +60,35 @@ class ExtraRc {
 		};
 		class distance400m {
 			text = "400 Meters";
-			script = "setViewDistance 400; systemChat('ViewDistance: 400'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 400; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 400'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance600m {
 			text = "600 Meters";
-			script = "setViewDistance 600; systemChat('ViewDistance: 600'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 600; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 600'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance800m {
 			text = "800 Meters";
-			script = "setViewDistance 800; systemChat('ViewDistance: 800'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 800; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 800'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance1000m {
 			text = "1000 Meters";
-			script = "setViewDistance 1000; systemChat('ViewDistance: 1000'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 1000; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 1000'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance1250m {
 			text = "1250 Meters";
-			script = "setViewDistance 1250; systemChat('ViewDistance: 1250'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 1250; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 1250'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance1500m {
 			text = "1500 Meters";
-			script = "setViewDistance 1500; systemChat('ViewDistance: 1500'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 1500; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 1500'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance2000m {
 			text = "2000 Meters";
-			script = "setViewDistance 2000; systemChat('ViewDistance: 2000'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 2000; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 2000'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 		class distance3000m {
 			text = "3000 Meters";
-			script = "setViewDistance 3000; systemChat('ViewDistance: 3000'); systemChat('Warning: Higher the view distance Lower the FPS');";
+			script = "setViewDistance 3000; player setVariable ['P2_viewDist', viewDistance, true]; systemChat('ViewDistance: 3000'); systemChat('Warning: Higher the view distance Lower the FPS');";
 		};
 	};
 	/*DJ Multi Skin Change */
